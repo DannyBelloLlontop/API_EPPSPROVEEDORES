b492f3002a7107dcd51f8ff87f53ea0a1e15faf0
DannyBelloLlontop <daniel.bello.llontop@gmail.com>
2023-02-04 17:48:26 -0500

    insert

--- controller/producto.php
+++ controller/producto.php
@@ -1,11 +1,21 @@
 <?php
+
+    header('Content-Type: application/json');
+
     require_once("../config/conexion.php");
     require_once("../models/Producto.php");
     $producto = new Producto();
 
+    $body = json_decode(file_get_contents("php://input"),true);
+
     switch($_GET["op"]){
         case "GetAll":
             $datos=$producto->get_producto();
+            echo json_encode($datos);
+        break;
+
+        case "GetId":
+            $datos=$producto->get_producto_x_id($body["producto_id"]);
             echo json_encode($datos);
         break;
     }
--- models/Producto.php
+++ models/Producto.php
@@ -8,5 +8,15 @@
             $sql->execute();
             return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
         }
+
+        public function get_producto_x_id($producto_id){
+            $conectar= parent::Conexion();
+            parent::set_names();
+            $sql="SELECT producto_descripcion, producto_color, producto_marca, producto_categoria from producto WHERE producto_estado = 1 AND producto_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $producto_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
 ?>
